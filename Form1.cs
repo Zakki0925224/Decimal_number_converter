a3636850f00326095a237c92b12ce9c6afaca5c5
Zakki <49384910+Zakki0925224@users.noreply.github.com>
2019-12-19 16:56:57 +0900

    Update Form1.cs

--- Form1.cs
+++ Form1.cs
@@ -17,11 +17,6 @@ namespace Decimal_number_comverter
             InitializeComponent();
         }
 
-        private void textBox1_TextChanged(object sender, EventArgs e)
-        {
-            
-        }
-
         private void button1_Click(object sender, EventArgs e)
         {
             //テキストボックスが空ならば警告を表示する
